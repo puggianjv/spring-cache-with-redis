f045354bc81ad46751252a9e57b9816dd2e8c064
Joao Puggian <puggianjv@gmail.com>
2021-05-19 07:16:56 -0300

    cache on service layer

--- src/main/java/com/puggian/springcachewithredis/service/PersonService.java
+++ src/main/java/com/puggian/springcachewithredis/service/PersonService.java
@@ -2,6 +2,8 @@ package com.puggian.springcachewithredis.service;
 
 import com.puggian.springcachewithredis.model.Person;
 import com.puggian.springcachewithredis.repository.PersonRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,17 +18,30 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
+    @CacheEvict(cacheNames = "Person",  allEntries = true)
     public Person save(String name) {
         Person person = new Person();
         person.setName(name);
         return personRepository.save(person);
     }
 
+    @Cacheable(cacheNames = "Person", key = "#id")
     public Optional<Person> find(long id) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return personRepository.findById(id);
     }
 
+    @Cacheable(cacheNames = "Person", key = "#root.method.name")
     public List<Person> findAll() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return personRepository.findAll();
     }
 }
